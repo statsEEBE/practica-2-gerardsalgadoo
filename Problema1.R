a6e3cdff7369fcc8c79db26a6f9844c5d99b628c
gerard.salgado <gerard.salgado@auleseebe.upc.edu>
2024-10-03 11:31:34 +0200

    Practica 2 probema 1

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,15 @@
 #Codigo para problema 1
 
+# Trobeu el coef. de determinació o de bondat d'ajust
+Rsq <- 0.192 #(multiple r-squared)
+R <- sqrt(Rsq)
+
+# Que variación espararemos del valor de mercado para 1 augmento de 1 unidad?
+m <- 0.0658
+
+# Predicción del valor para activos = 647.1
+m <- 0.0658
+b <- 11.768
+a <- 647.1
+y <- m*a + b
+y
